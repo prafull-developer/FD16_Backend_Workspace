cad8a1708d7376c4f9010d73978298cbf756bd37
prafull-developer <prafullshete815@gmail.com>
2023-10-13 17:24:48 +0530

    Parafull Changes

--- re-service/src/main/java/com/delta/ReServiceApplication.java
+++ re-service/src/main/java/com/delta/ReServiceApplication.java
@@ -9,6 +9,7 @@ public class ReServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReServiceApplication.class, args);
 		System.out.println("Abhimanyu Changes");
+		System.out.println("Praful Changes");
 	}
 
 }
