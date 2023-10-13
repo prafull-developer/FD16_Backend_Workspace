257732eb944343affaae7c78e874b069ec2ebced
prafull <hearmeroar009@gmail.com>
2023-10-13 16:50:19 +0530

    re-service created

---
+++ re-service/src/main/java/com/delta/ReServiceApplication.java
@@ -0,0 +1,13 @@
+package com.delta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReServiceApplication.class, args);
+	}
+
+}
